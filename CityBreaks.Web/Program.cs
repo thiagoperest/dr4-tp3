87c69afb7598c399f5edf8929f77591aef2baa59
thiagoperest <thiago_teodoroperes07@hotmail.com>
2025-09-05 09:45:41 -0300

    fix: ex1 - ajuste BD para SQ Lite para compatibilidade arm

--- CityBreaks.Web/Program.cs
+++ CityBreaks.Web/Program.cs
@@ -6,17 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-// Add Entity Framework
+// Add Entity Framework com SQLite
 builder.Services.AddDbContext<CityBreaksContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// Criar banco automaticamente se não existir
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<CityBreaksContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
