9b7c6308151a7767f9405627adeeaa218c550d3c
thiagoperest <thiago_teodoroperes07@hotmail.com>
2025-09-07 09:16:55 -0300

    feat: ex10 - atualizando Registros com segurança

--- CityBreaks.Web/Services/IPropertyService.cs
+++ CityBreaks.Web/Services/IPropertyService.cs
@@ -5,5 +5,7 @@ namespace CityBreaks.Web.Services
     public interface IPropertyService
     {
         Task<bool> CreateAsync(Property property);
+        Task<Property?> GetByIdAsync(int id);
+        Task<bool> UpdateAsync(int id, string name, decimal pricePerNight, int cityId);
     }
 }
--- CityBreaks.Web/Services/PropertyService.cs
+++ CityBreaks.Web/Services/PropertyService.cs
@@ -1,5 +1,6 @@
 using CityBreaks.Web.Data;
 using CityBreaks.Web.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityBreaks.Web.Services
 {
@@ -17,6 +18,35 @@ namespace CityBreaks.Web.Services
             try
             {
                 await _context.Properties.AddAsync(property);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<Property?> GetByIdAsync(int id)
+        {
+            return await _context.Properties
+                .Include(p => p.City)
+                .ThenInclude(c => c.Country)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> UpdateAsync(int id, string name, decimal pricePerNight, int cityId)
+        {
+            try
+            {
+                var property = await _context.Properties.FindAsync(id);
+                if (property == null)
+                    return false;
+
+                property.Name = name;
+                property.PricePerNight = pricePerNight;
+                property.CityId = cityId;
+
                 await _context.SaveChangesAsync();
                 return true;
             }
